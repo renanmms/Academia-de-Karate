706af28c064490802282a19bf2cea5a9eb94478c
Renan Martins <renanmartins1999@hotmail.com>
2022-07-04 11:05:45 -0300

    Adicionar arquivos de projeto.

---
+++ Academia_de_Karate/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Academia_de_Karate.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Academia_de_Karate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Academia_de_Karate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Academia_de_Karate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
