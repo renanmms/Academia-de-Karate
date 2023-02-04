c3aa79d3d9bec2ec85c1c83c1b632db68c0f99fc
Renan Martins <renanmartins1999@hotmail.com>
2023-02-04 20:30:57 -0300

    Adicionando modelo do aluno

---
+++ Academia_de_Karate/Models/AlunoInputModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Academia_de_Karate.Models
+{
+    public class AlunoInputModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
