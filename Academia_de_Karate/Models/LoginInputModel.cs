71a82f6d4c929e77c80fe199c53578cdf2520ff7
Renan Martins <renanmartins1999@hotmail.com>
2023-04-28 23:51:22 -0300

    Adicionando validações na model de login

--- Academia_de_Karate/Models/LoginInputModel.cs
+++ Academia_de_Karate/Models/LoginInputModel.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Academia_de_Karate.Models
 {
     public class LoginInputModel
     {
+        [Required(ErrorMessage = "Login é Obrigatório!")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Senha é Obrigatória!")]
         public string Senha { get; set; }
     }
 }
